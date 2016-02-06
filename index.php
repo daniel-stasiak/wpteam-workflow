e6924fa2b3e3b4aace6aede8ebdf1adbe380a4df
agencja-acclaim <hello@acclaim.pl>
2016-02-06 12:33:42 +0100

    Guplfile change for scripts

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
         <![endif]-->
 
         <!-- Add your site or application content here -->
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-3 col-xs-offset-6">
+                    pamsss
+                </div><!-- /.col-xs-3 col-xs-offset-6 -->
+            </div><!-- /.row -->
+            test
+        </div><!-- /.container -->
         <p>Hello world! This is HTML5 Boilerplate.</p>
 
 <?php include "footer.php"; ?>
