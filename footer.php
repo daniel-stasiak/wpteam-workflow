255efb74f071605fd3967b01e1684e9c5efe7143
agencja-acclaim <hello@acclaim.pl>
2016-02-06 12:37:12 +0100

    Footer change
    Adding WOW.js init

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
-        <!--<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>-->
 
         <script src="scripts/scripts.min.js"></script>
+
+        <script>
+            new WOW().init();
+        </script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
