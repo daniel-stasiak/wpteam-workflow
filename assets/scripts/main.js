0c45d6025d1d3e6813634767e07e37bb51df0bbd
agencja-acclaim <hello@acclaim.pl>
2016-02-28 22:24:27 +0100

    Few general changes

---
+++ assets/scripts/main.js
@@ -0,0 +1,27 @@
+$(function() {
+
+    /* ~~~~~~~~~~ Animsition ~~~~~~~~~~ */
+
+    $(document).ready(function() {
+        $(".animsition").animsition({
+            inClass: 'fade-in',
+            outClass: 'fade-out',
+            inDuration: 1500,
+            outDuration: 800,
+            linkElement: '.animsition-link',
+            loading: true,
+            loadingParentElement: 'html',
+            loadingClass: 'animsition-loading',
+            loadingInner: '',
+            timeout: false,
+            timeoutCountdown: 5000,
+            onLoadEvent: true,
+            browser: [ 'animation-duration', '-webkit-animation-duration'],
+            overlay : false,
+            overlayClass : 'animsition-overlay-slide',
+            overlayParentElement : 'html',
+            transition: function(url){ window.location.href = url; }
+        });
+    });
+
+});
--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php include "header.php"; ?>
-    <body>
+    <body class="animsition">
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-
-        <!-- Add your site or application content here -->
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-3 col-xs-offset-6">
-                    pamsss
-                </div><!-- /.col-xs-3 col-xs-offset-6 -->
-            </div><!-- /.row -->
-            test
-        </div><!-- /.container -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
 
 <?php include "footer.php"; ?>
