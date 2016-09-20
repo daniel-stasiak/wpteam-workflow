08dec53b71f6cc1bc7f9b9509bae2d156a866866
agencja-acclaim <hello@acclaim.pl>
2016-09-20 15:48:24 +0200

    Updated Bootstrap to Alpha 4, Move the homepage to the template

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
-<?php get_header(); ?>
-
-<main id="main" class="homepage" tabindex="-1">
-
-	<h1>Hello Word</h1>
-
-</main><!-- /#main.homepage -->
-
-<?php get_footer(); ?>
+<?php require_once('templates/homepage.php'); ?>
---
+++ templates/homepage.php
@@ -0,0 +1,15 @@
+<?php
+    /*
+        Template Name: Homepage
+    */
+?>
+
+<?php get_header(); ?>
+
+<main id="main" class="homepage" tabindex="-1">
+
+	<h1>Hello Word</h1>
+
+</main><!-- /#main.homepage -->
+
+<?php get_footer(); ?>
