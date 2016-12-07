296cdd9c30db468a641b50a0b0c3d81b6f1f0b4f
agencja-acclaim <hello@acclaim.pl>
2016-12-07 09:37:40 +0100

    Changed name of main to homepage-template

--- templates/homepage.php
+++ templates/homepage.php
@@ -6,10 +6,10 @@
 
 <?php get_header(); ?>
 
-<main id="main" class="homepage" tabindex="-1">
+<main id="main" class="homepage-template" tabindex="-1">
 
 	<h1>Hello Word</h1>
 
-</main><!-- /#main.homepage -->
+</main><!-- /#main.homepage-template -->
 
 <?php get_footer(); ?>
