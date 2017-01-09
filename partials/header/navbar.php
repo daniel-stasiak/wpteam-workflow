857f9efcfe1f429239b43b10373481f1f93816ed
agencja-acclaim <hello@acclaim.pl>
2017-01-09 12:42:34 +0100

    Navbar updated to match Bootstrap 4 Alpha 6

--- partials/header/navbar.php
+++ partials/header/navbar.php
@@ -1,35 +1,31 @@
 <nav class="navbar">
     <h2 class="hide">Main navigation</h2>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <ul class="grid flexbox flexbox--space-between">
-                    <li>
-                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand animsition-link">
-                            <?php //echo file_get_contents(get_template_directory_uri().'/images/logo__XXX.svg'); ?>
-                        </a>
-                    </li>
-                    <li>
-                        <?php
-                            // wp_nav_menu([
-                            //     'menu'            => 'Main - navigation',
-                            //     'theme_location'  => 'main_navigation',
-                            //     'container'       => false,
-                            //     'menu_id'         => false,
-                            //     'menu_class'      => 'navbar-nav mr-auto',
-                            //     'depth'           => 2,
-                            //     'fallback_cb'     => 'bs4navwalker::fallback',
-                            //     'walker'          => new bs4navwalker()
-                            // ]);
-                        ?>
-                        <button id="mmenu-triger" class="hamburger hamburger--squeeze" type="button" aria-label="Menu">
-                            <span class="hamburger-box">
-                                <span class="hamburger-inner"></span>
-                            </span>
-                        </button>
-                    </li>
-                </ul><!-- /.grid flexbox flexbox--space-between -->
-            </div><!-- /.col-xs-12 -->
-        </div><!-- /.row -->
+        <div class="row align-items-center justify-content-between">
+            <div class="col col-auto">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand animsition-link">
+                    <?php //echo file_get_contents(get_template_directory_uri().'/images/logo__XXX.svg'); ?>
+                </a>
+            </div><!-- /.col col-auto -->
+            <div class="col col-auto">
+                <?php
+                    // wp_nav_menu([
+                    //     'menu'            => 'Main - navigation',
+                    //     'theme_location'  => 'main_navigation',
+                    //     'container'       => false,
+                    //     'menu_id'         => false,
+                    //     'menu_class'      => 'navbar-nav mr-auto',
+                    //     'depth'           => 2,
+                    //     'fallback_cb'     => 'bs4navwalker::fallback',
+                    //     'walker'          => new bs4navwalker()
+                    // ]);
+                ?>
+                <button id="mmenu-triger" class="hamburger hamburger--squeeze" type="button" aria-label="Menu">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div><!-- /.col col-auto -->
+        </div><!-- /.row align-items-center justify-content-between -->
     </div><!-- /.container -->
 </nav><!-- /.navbar -->
