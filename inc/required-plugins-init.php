ac60778619c9228a85a1fb4eddb95f506485e3c2
agencja-acclaim <hello@acclaim.pl>
2017-02-21 12:57:46 +0100

    Added Remove Query Strings From Static Resources plugin to required plugins

--- inc/required-plugins-init.php
+++ inc/required-plugins-init.php
@@ -216,6 +216,18 @@ function wp_team_register_required_plugins() {
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
 
+		array(
+			'name'               => 'Remove Query Strings From Static Resources', // The plugin name.
+			'slug'               => 'remove-query-strings-from-static-resources', // The plugin slug (typically the folder name).
+			'source'             => 'https://downloads.wordpress.org/plugin/remove-query-strings-from-static-resources.1.3.1.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+
 	);
 
 	/*
