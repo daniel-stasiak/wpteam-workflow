e8ddf72441bcb6dde872b03bf64682acde8c69cc
weareacclaim <you@example.com>
2017-03-06 17:14:43 +0100

    Required plugins update

--- inc/required-plugins-init.php
+++ inc/required-plugins-init.php
@@ -207,7 +207,7 @@ function wp_team_register_required_plugins() {
 		array(
 			'name'               => 'WP Smush', // The plugin name.
 			'slug'               => 'wp-smush', // The plugin slug (typically the folder name).
-			'source'             => 'https://github.com/wp-plugins/wp-smushit/archive/master.zip', // The plugin source.
+			'source'             => 'https://downloads.wordpress.org/plugin/wp-smushit.2.6.1.zip', // The plugin source.
 			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
@@ -220,6 +220,18 @@ function wp_team_register_required_plugins() {
 			'name'               => 'Remove Query Strings From Static Resources', // The plugin name.
 			'slug'               => 'remove-query-strings-from-static-resources', // The plugin slug (typically the folder name).
 			'source'             => 'https://downloads.wordpress.org/plugin/remove-query-strings-from-static-resources.1.3.1.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+
+		array(
+			'name'               => 'GTmetrix for WordPress', // The plugin name.
+			'slug'               => 'gtmetrix-for-wordpress', // The plugin slug (typically the folder name).
+			'source'             => 'https://downloads.wordpress.org/plugin/gtmetrix-for-wordpress.0.4.2.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
