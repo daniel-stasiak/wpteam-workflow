5d0b1af0783005b12c304a08dcf3ef01284861d9
weareacclaim <you@example.com>
2017-03-13 10:08:16 +0100

    Removed Archive Menu plugin

--- inc/required-plugins-init.php
+++ inc/required-plugins-init.php
@@ -169,18 +169,6 @@ function wp_team_register_required_plugins() {
 		),
 
 		array(
-			'name'               => 'WordPress Post Type Archive Links', // The plugin name.
-			'slug'               => 'post-type-archive-links', // The plugin slug (typically the folder name).
-			'source'             => 'https://github.com/stephenharris/WordPress-Post-Type-Archive-Links/archive/master.zip', // The plugin source.
-			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),
-
-		array(
 			'name'               => 'SVG Support', // The plugin name.
 			'slug'               => 'svg-support', // The plugin slug (typically the folder name).
 			'source'             => 'https://github.com/wp-plugins/svg-support/archive/master.zip', // The plugin source.
