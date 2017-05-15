e34c106a999292b0cc805448d13e00c3b96dcb7f
weareacclaim <you@example.com>
2017-05-15 08:10:13 +0200

    Workflow updates

--- partials/header/navbar.php
+++ partials/header/navbar.php
@@ -4,7 +4,7 @@
         <div class="row align-items-center justify-content-between">
             <div class="col col-auto">
                 <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="navbar-brand animsition-link">
-                    <?php //echo file_get_contents(get_template_directory_uri().'/images/logo__XXX.svg'); ?>
+                    <?php //echo file_get_contents(get_template_directory().'/images/logo__XXX.svg'); ?>
                 </a>
             </div><!-- /.col col-auto -->
             <div class="col col-auto">
