d64e81442831ec37b7e3ea52700bdc06347dd295
weareacclaim <you@example.com>
2017-06-28 12:07:44 +0200

    Set minimum size for uploaded featured image function added

--- functions.php
+++ functions.php
@@ -106,6 +106,27 @@ endif;
 // add_image_size( 'image-type-title', 'X', 'X', true);
 
 
+/* ~~~~~~~~~~~ Set minimum size for uploaded featured image ~~~~~~~~~~ */
+
+// add_filter('wp_handle_upload_prefilter','tc_handle_upload_prefilter');
+// function tc_handle_upload_prefilter($file)
+// {
+
+//     $img=getimagesize($file['tmp_name']);
+//     $minimum = array('width' => '640', 'height' => '480');
+//     $width= $img[0];
+//     $height =$img[1];
+
+//     if ($width < $minimum['width'] )
+//         return array("error"=>"Image dimensions are too small. Minimum width is {$minimum['width']}px. Uploaded image width is $width px");
+
+//     elseif ($height <  $minimum['height'])
+//         return array("error"=>"Image dimensions are too small. Minimum height is {$minimum['height']}px. Uploaded image height is $height px");
+//     else
+//         return $file;
+// }
+
+
 /* ~~~~~~~~~~ Protection for e-mail addresses in html ~~~~~~~~~~ */
 
 add_filter('acf/load_value', 'eae_encode_emails');
