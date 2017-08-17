654e6694c716bee24c31c22c4cf4801a20d82ef2
weareacclaim <you@example.com>
2017-08-17 17:08:53 +0200

    Required plugins update

--- inc/required-plugins-init.php
+++ inc/required-plugins-init.php
@@ -133,9 +133,9 @@ function wp_team_register_required_plugins() {
 		),
 
 		array(
-			'name'               => 'Post Duplicator', // The plugin name.
-			'slug'               => 'post-duplicator', // The plugin slug (typically the folder name).
-			'source'             => 'https://github.com/meta4creations/post-duplicator/archive/master.zip', // The plugin source.
+			'name'               => 'Duplicate Post', // The plugin name.
+			'slug'               => 'duplicate-post', // The plugin slug (typically the folder name).
+			'source'             => 'https://downloads.wordpress.org/plugin/duplicate-post.3.2.zip', // The plugin source.
 			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
