28f0332016da5d2c543a643a6d7fa600f45c5e5c
weareacclaim <you@example.com>
2017-09-14 07:52:12 +0200

    Options Page Updates

--- functions.php
+++ functions.php
@@ -3,31 +3,31 @@
 /* ~~~~~~~~~~ Add options page to Wordpress with ACF ~~~~~~~~~ */
 
 // if( function_exists('acf_add_options_page') ) {
-// 	acf_add_options_page(array(
-// 		'page_title' 	=> 'Theme title',
-// 		'menu_title'	=> 'Theme title',
-// 		'menu_slug' 	=> 'theme-general-settings',
-// 		'capability'	=> 'edit_posts',
-// 		'redirect'		=> false
-// 	));
+//     acf_add_options_page(array(
+//         'page_title'    => get_bloginfo('name'),
+//         'menu_title'    => get_bloginfo('name'),
+//         'menu_slug'     => 'theme-general-settings',
+//         'capability'    => 'edit_posts',
+//         'redirect'      => false
+//     ));
 
-// 	acf_add_options_sub_page(array(
-// 		'page_title' 	=> 'Home',
-// 		'menu_title'	=> 'Home',
-// 		'parent_slug'	=> 'theme-general-settings',
-// 	));
+//     acf_add_options_sub_page(array(
+//         'page_title'    => 'Home',
+//         'menu_title'    => 'Home',
+//         'parent_slug'   => 'theme-general-settings',
+//     ));
 // }
 
 
 /* ~~~~~~~~~~ Add custom Wordpress navigation ~~~~~~~~~~ */
 
-if(function_exists('register_nav_menus')) {
-	register_nav_menus(
-		array(
-			// 'main_navigation' => 'Main Navigation'
-		)
-	);
-}
+// if(function_exists('register_nav_menus')) {
+// 	register_nav_menus(
+// 		array(
+// 			'main_navigation' => 'Main Navigation'
+// 		)
+// 	);
+// }
 
 
 /* ~~~~~~~~~~ Widget areas ~~~~~~~~~~ */
