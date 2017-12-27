e624396bbecbb9b2f722ae05c1cdc34f6396cc44
dawidstasiak <dawidstasiak@dawidstasiak.pl>
2017-12-27 16:11:01 +0100

    1. OWL Placeholder added 2. Added Require Featured Image Plugin to required plugins list

--- inc/required-plugins-init.php
+++ inc/required-plugins-init.php
@@ -228,6 +228,18 @@ function wp_team_register_required_plugins() {
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
 
+		array(
+			'name'               => 'Require Featured Image', // The plugin name.
+			'slug'               => 'require-featured-image', // The plugin slug (typically the folder name).
+			'source'             => 'https://downloads.wordpress.org/plugin/require-featured-image.1.3.0.zip', // The plugin source.
+			'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+
 	);
 
 	/*
