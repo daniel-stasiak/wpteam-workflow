988bbfe7ba181c57530652b4cf394842a477f79d
dawidstasiak <dawidstasiak@dawidstasiak.pl>
2018-02-04 14:13:26 +0100

    Removed Lazy Loading from Logo

--- partials/footer/main-footer.php
+++ partials/footer/main-footer.php
@@ -5,3 +5,4 @@
 </div><!-- /.footer-wrapper -->
 
 <!-- © <?php //echo date('Y').' '.get_bloginfo('name'); ?> -->
+<!-- <img data-src="<?php //echo get_template_directory_uri(); ?>/images/logo__XXX.svg" src="<?php //echo get_template_directory_uri(); ?>/images/img__empty.png" alt="<?php //echo get_bloginfo('name'); ?>" class="navbar-brand__logo lazy" />-->
